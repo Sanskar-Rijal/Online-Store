8ed33ae58b1ece6a1e2709b210775c180a8a72b7
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-12-28 10:27:35 +0545

    created model for order

---
+++ Backend/Models/order.js
@@ -0,0 +1,115 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: [true, "Address field is required"],
+      },
+      city: {
+        type: String,
+        required: [true, "City field is required"],
+      },
+      phoneNo: {
+        type: String,
+        required: [true, "Phone number field is required"],
+      },
+      postalCode: {
+        type: String,
+        required: [true, "Postal code field is required"],
+      },
+      country: {
+        type: String,
+        required: [true, "Country field is required"],
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Order must belong to a user"],
+      ref: "User",
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: [true, "Please enter product name"],
+        },
+        quantity: {
+          type: Number,
+          required: [true, "Please enter product quantity"],
+        },
+        image: {
+          type: String,
+          required: [true, "Please enter product image"],
+        },
+        price: {
+          type: Number,
+          required: [true, "Product price is required"],
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: [true, "Order. items must have a product"],
+          ref: "Product",
+        },
+      },
+    ],
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+    },
+    paymentMethod: {
+      type: String,
+      required: [true, "Please enter payment method"],
+      enum: {
+        values: ["COD", "Online"],
+        message: "Please select correct payment method",
+      },
+    },
+    itemsPrice: {
+      type: Number,
+      required: [true, "Please enter item Price"],
+      defaule: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: [true, "Please enter tax price"],
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0.0,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, "Please enter total price"],
+      default: 0.0,
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+      default: "Processing",
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
